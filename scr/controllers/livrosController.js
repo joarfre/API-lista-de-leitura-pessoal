9dd4fbe73c61d01c1feb03583b0f6123f3768dbd
Jonathan Freitas <jonathan_freitas@id.uff.br>
2022-12-05 02:13:36 -0300

    adicionando novos arquivos que serão necessários e feita a primeira parte do código de livrosController

---
+++ scr/controllers/livrosController.js
@@ -0,0 +1,36 @@
+import livros from "../models/Livros.js";
+
+class LivroController {
+    static listarLivros = (req, res) => {
+        livros.find()
+        .exec((err, livros) => {
+            res.status(200).json(livros)
+        })
+    }
+
+    static cadastrarLivros = (req, res) => {
+        let livro = new livros (req.body);
+
+        livro.save((err) => {
+            if(err) {
+                res.status(500).send({message:`${err.message} - não foi possível cadastrar o livro.`})
+            } else {
+                res.status(200).send(livro.toJSON())
+            }
+        })
+    }
+
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(err) {
+                res.status(500).send({message: err.message})
+            } else {
+                res.status(200).send({message: 'Livro atualizado'})
+            }
+        })
+    }
+}
+
+export default LivroController
