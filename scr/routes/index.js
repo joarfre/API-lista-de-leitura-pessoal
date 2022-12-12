520ec75830b5002e7c2848adb443e321e16a3c7a
Jonathan Freitas <jonathan_freitas@id.uff.br>
2022-12-12 15:07:34 -0300

    corrigindo o index.js da pasta routes. Alteração de 'app.routes' para 'app.route'

--- scr/routes/index.js
+++ scr/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import livros from "./livrosRoutes.js";
 
 const routes = (app) => {
-    app.routes('/').get((req,res) =>{
+    app.route('/').get((req,res) =>{
         res.status(200).send({titulo: "livros para leitura"})
     })
 
